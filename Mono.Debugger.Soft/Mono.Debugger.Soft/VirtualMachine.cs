8d43e244e8ea07faca98812a5c515f9dc859ac08
Jeffrey Stedfast <jeff@xamarin.com>
2014-05-14 10:19:38 -0400

    [Mono.Debugger.Soft] updated

--- Mono.Debugger.Soft/Mono.Debugger.Soft/VirtualMachine.cs
+++ Mono.Debugger.Soft/Mono.Debugger.Soft/VirtualMachine.cs
@@ -442,6 +442,13 @@ namespace Mono.Debugger.Soft
 			}
 	    }
 
+		internal void InvalidateAssemblyCaches () {
+			lock (domains_lock) {
+				foreach (var d in domains.Values)
+					d.InvalidateAssembliesCache ();
+			}
+		}
+
 		Dictionary <long, TypeMirror> types;
 		object types_lock = new object ();
 
@@ -560,6 +567,10 @@ namespace Mono.Debugger.Soft
 		}
 
 		internal Value DecodeValue (ValueImpl v) {
+			return DecodeValue (v, null);
+		}
+
+		internal Value DecodeValue (ValueImpl v, Dictionary<int, Value> parent_vtypes) {
 			if (v.Value != null)
 				return new PrimitiveValue (this, v.Value);
 
@@ -575,12 +586,21 @@ namespace Mono.Debugger.Soft
 			case ElementType.Object:
 				return GetObject (v.Objid);
 			case ElementType.ValueType:
+				if (parent_vtypes == null)
+					parent_vtypes = new Dictionary<int, Value> ();
+				StructMirror vtype;
 				if (v.IsEnum)
-					return new EnumMirror (this, GetType (v.Klass), DecodeValues (v.Fields));
+					vtype = new EnumMirror (this, GetType (v.Klass), (Value[])null);
 				else
-					return new StructMirror (this, GetType (v.Klass), DecodeValues (v.Fields));
+					vtype = new StructMirror (this, GetType (v.Klass), (Value[])null);
+				parent_vtypes [parent_vtypes.Count] = vtype;
+				vtype.SetFields (DecodeValues (v.Fields, parent_vtypes));
+				parent_vtypes.Remove (parent_vtypes.Count - 1);
+				return vtype;
 			case (ElementType)ValueTypeId.VALUE_TYPE_ID_NULL:
 				return new PrimitiveValue (this, null);
+			case (ElementType)ValueTypeId.VALUE_TYPE_ID_PARENT_VTYPE:
+				return parent_vtypes [v.Index];
 			default:
 				throw new NotImplementedException ("" + v.Type);
 			}
@@ -590,6 +610,13 @@ namespace Mono.Debugger.Soft
 			Value[] res = new Value [values.Length];
 			for (int i = 0; i < values.Length; ++i)
 				res [i] = DecodeValue (values [i]);
+			return res;
+		}
+
+		internal Value[] DecodeValues (ValueImpl[] values, Dictionary<int, Value> parent_vtypes) {
+			Value[] res = new Value [values.Length];
+			for (int i = 0; i < values.Length; ++i)
+				res [i] = DecodeValue (values [i], parent_vtypes);
 			return res;
 		}
 
@@ -654,12 +681,12 @@ namespace Mono.Debugger.Soft
 					l.Add (new ThreadDeathEvent (vm, req_id, id));
 					break;
 				case EventType.AssemblyLoad:
+					vm.InvalidateAssemblyCaches ();
 					l.Add (new AssemblyLoadEvent (vm, req_id, thread_id, id));
-					vm.GetThread (thread_id).Domain.InvalidateAssembliesCache ();
 					break;
 				case EventType.AssemblyUnload:
+					vm.InvalidateAssemblyCaches ();
 					l.Add (new AssemblyUnloadEvent (vm, req_id, thread_id, id));
-					vm.GetThread (thread_id).Domain.InvalidateAssembliesCache ();
 					break;
 				case EventType.TypeLoad:
 					l.Add (new TypeLoadEvent (vm, req_id, thread_id, id));
