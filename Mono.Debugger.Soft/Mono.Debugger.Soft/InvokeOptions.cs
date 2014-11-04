b35d6f6f6f9465772581f15f0734034e1072eaa6
Jeffrey Stedfast <jeff@xamarin.com>
2014-11-04 13:55:39 -0500

    [Mono.Debugger.Soft] sync'd up with Mono master

--- Mono.Debugger.Soft/Mono.Debugger.Soft/InvokeOptions.cs
+++ Mono.Debugger.Soft/Mono.Debugger.Soft/InvokeOptions.cs
@@ -13,6 +13,20 @@ namespace Mono.Debugger.Soft
 		/*
 		 * Only resume the target thread during the invoke
 		 */
-		SingleThreaded = 2
+		SingleThreaded = 2,
+		/*
+		 * Return the changed receiver when invoking
+		 * a valuetype method.
+		 */
+		ReturnOutThis = 4,
+		/*
+		 * Return the values of out arguments
+		 */
+		ReturnOutArgs = 8,
+		/*
+		 * Do a virtual invoke
+		 * Since protocol version 2.37
+		 */
+		Virtual = 16
 	}
 }
