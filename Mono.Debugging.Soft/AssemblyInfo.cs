291e6c7e07e9ed10f35e14d1e4ad654ab9a9e476
Artem.Bukhonov <Artem.Bukhonov@jetbrains.com>
2015-09-21 21:56:06 +0300

    Versions++

--- Mono.Debugging.Soft/AssemblyInfo.cs
+++ Mono.Debugging.Soft/AssemblyInfo.cs
@@ -27,5 +27,5 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 
 [assembly: AssemblyTitle("Mono.Debugging.Soft")]
-[assembly: AssemblyVersionAttribute("1.0.0.5")]
-[assembly: AssemblyFileVersionAttribute("1.0.0.5")]
+[assembly: AssemblyVersionAttribute("1.0.0.6")]
+[assembly: AssemblyFileVersionAttribute("1.0.0.6")]
