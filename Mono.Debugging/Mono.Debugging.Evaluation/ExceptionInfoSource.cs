6c64bd1e403e6313ab006de97f1218f98becca7d
David Karlaš <david.karlas@xamarin.com>
2016-05-24 15:48:03 +0200

    Fixed fetching inner exceptions Problem was that calling Exception.GetChild("property name") failed because since commit 3cf1e47d we are using member type(field, property..) to get value, to be more respectful regarding casting... And since we use enumerator to get InnerExceptions which is returning System.Object we failed.. way to fix this is to explicitly set type to object type...

--- Mono.Debugging/Mono.Debugging.Evaluation/ExceptionInfoSource.cs
+++ Mono.Debugging/Mono.Debugging.Evaluation/ExceptionInfoSource.cs
@@ -36,61 +36,64 @@ namespace Mono.Debugging.Evaluation
 {
 	public class ExceptionInfoSource
 	{
-		readonly EvaluationContext ctx;
-		
+		EvaluationContext ctx;
+
 		public ExceptionInfoSource (EvaluationContext ctx, ValueReference exception)
 		{
 			Exception = exception;
 			this.ctx = ctx;
 		}
-		
+
 		public ValueReference Exception {
 			get; private set;
 		}
-		
+
 		public ObjectValue CreateObjectValue (bool withTimeout, EvaluationOptions options)
 		{
 			options = options.Clone ();
 			options.EllipsizeStrings = false;
+			options.AllowTargetInvoke = true;
+			ctx = ctx.WithOptions (options);
 			string type = ctx.Adapter.GetValueTypeName (ctx, Exception.ObjectValue);
 
 			var excInstance = Exception.CreateObjectValue (withTimeout, options);
 			excInstance.Name = "Instance";
-			
+
 			ObjectValue messageValue = null;
+			var exceptionType = ctx.Adapter.GetValueType (ctx, Exception.Value);
 
 			// Get the message
-			
+
 			if (withTimeout) {
 				messageValue = ctx.Adapter.CreateObjectValueAsync ("Message", ObjectValueFlags.None, delegate {
-					var mref = Exception.GetChild ("Message", options);
+					var mref = ctx.Adapter.GetMember (ctx, Exception, exceptionType, Exception.Value, "Message");
 					if (mref != null) {
-						string val = (string) mref.ObjectValue;
+						string val = (string)mref.ObjectValue;
 						return ObjectValue.CreatePrimitive (null, new ObjectPath ("Message"), "string", new EvaluationResult (val), ObjectValueFlags.Literal);
 					}
 
 					return ObjectValue.CreateUnknown ("Message");
 				});
 			} else {
-				var mref = Exception.GetChild ("Message", options);
+				var mref = ctx.Adapter.GetMember (ctx, Exception, exceptionType, Exception.Value, "Message");
 				if (mref != null) {
-					string val = (string) mref.ObjectValue;
+					string val = (string)mref.ObjectValue;
 					messageValue = ObjectValue.CreatePrimitive (null, new ObjectPath ("Message"), "string", new EvaluationResult (val), ObjectValueFlags.Literal);
 				}
 			}
 
 			if (messageValue == null)
 				messageValue = ObjectValue.CreateUnknown ("Message");
-			
+
 			messageValue.Name = "Message";
 
 			// Inner exception
-			
+
 			ObjectValue childExceptionValue = null;
-			
+
 			if (withTimeout) {
 				childExceptionValue = ctx.Adapter.CreateObjectValueAsync ("InnerException", ObjectValueFlags.None, delegate {
-					var inner = Exception.GetChild ("InnerException", options);
+					var inner = ctx.Adapter.GetMember (ctx, Exception, exceptionType, Exception.Value, "InnerException");
 					if (inner != null && !ctx.Adapter.IsNull (ctx, inner.Value)) {
 						//Console.WriteLine ("pp got child:" + type);
 						var innerSource = new ExceptionInfoSource (ctx, inner);
@@ -101,7 +104,7 @@ namespace Mono.Debugging.Evaluation
 					return ObjectValue.CreateUnknown ("InnerException");
 				});
 			} else {
-				var inner = Exception.GetChild ("InnerException", options);
+				var inner = ctx.Adapter.GetMember (ctx, Exception, exceptionType, Exception.Value, "InnerException");
 				if (inner != null && !ctx.Adapter.IsNull (ctx, inner.Value)) {
 					//Console.WriteLine ("pp got child:" + type);
 					var innerSource = new ExceptionInfoSource (ctx, inner);
@@ -117,14 +120,14 @@ namespace Mono.Debugging.Evaluation
 
 			ObjectValue childExceptionsValue = null;
 			ObjectEvaluatorDelegate getInnerExceptionsDelegate = delegate {
-				var inner = Exception.GetChild ("InnerExceptions", options);
+				var inner = ctx.Adapter.GetMember (ctx, Exception, exceptionType, Exception.Value, "InnerExceptions");
 				if (inner != null && !ctx.Adapter.IsNull (ctx, inner.Value)) {
 					var obj = inner.GetValue (ctx);
 					var objType = ctx.Adapter.GetValueType (ctx, obj);
-					var enumerator = ctx.Adapter.RuntimeInvoke (ctx, objType, obj, "GetEnumerator", new object[0], new object[0]);
+					var enumerator = ctx.Adapter.RuntimeInvoke (ctx, objType, obj, "GetEnumerator", new object [0], new object [0]);
 					var enumeratorType = ctx.Adapter.GetImplementedInterfaces (ctx, ctx.Adapter.GetValueType (ctx, enumerator)).First (f => ctx.Adapter.GetTypeName (ctx, f) == "System.Collections.IEnumerator");
 					var childrenList = new List<ObjectValue> ();
-					while ((bool)ctx.Adapter.TargetObjectToObject (ctx, ctx.Adapter.RuntimeInvoke (ctx, enumeratorType, enumerator, "MoveNext", new object[0], new object[0]))) {
+					while ((bool)ctx.Adapter.TargetObjectToObject (ctx, ctx.Adapter.RuntimeInvoke (ctx, enumeratorType, enumerator, "MoveNext", new object [0], new object [0]))) {
 						var valCurrent = ctx.Adapter.GetMember (ctx, null, enumeratorType, enumerator, "Current");
 						childrenList.Add (new ExceptionInfoSource (ctx, valCurrent).CreateObjectValue (withTimeout, ctx.Options));
 					}
@@ -141,29 +144,29 @@ namespace Mono.Debugging.Evaluation
 
 			if (childExceptionsValue == null)
 				childExceptionsValue = ObjectValue.CreateUnknown ("InnerExceptions");
-			
+
 			// Stack trace
-			
+
 			ObjectValue stackTraceValue;
 			if (withTimeout) {
 				stackTraceValue = ctx.Adapter.CreateObjectValueAsync ("StackTrace", ObjectValueFlags.None, delegate {
-					return GetStackTrace (options);
+					return GetStackTrace (options, exceptionType);
 				});
 			} else {
-				stackTraceValue = GetStackTrace (options);
+				stackTraceValue = GetStackTrace (options, exceptionType);
 			}
 
 			var children = new ObjectValue [] { excInstance, messageValue, stackTraceValue, childExceptionValue, childExceptionsValue };
 
 			return ObjectValue.CreateObject (null, new ObjectPath ("InnerException"), type, "", ObjectValueFlags.None, children);
 		}
-		
-		ObjectValue GetStackTrace (EvaluationOptions options)
+
+		ObjectValue GetStackTrace (EvaluationOptions options, object exceptionType)
 		{
-			var stackTrace = Exception.GetChild ("StackTrace", options);
+			var stackTrace = ctx.Adapter.GetMember (ctx, Exception, exceptionType, Exception.Value, "StackTrace");
 			if (stackTrace == null)
 				return ObjectValue.CreateUnknown ("StackTrace");
-			
+
 			string trace = stackTrace.ObjectValue as string;
 			if (trace == null)
 				return ObjectValue.CreateUnknown ("StackTrace");
@@ -171,7 +174,7 @@ namespace Mono.Debugging.Evaluation
 			var regex = new Regex ("at (?<MethodName>.*) in (?<FileName>.*):(?<LineNumber>\\d+)(,(?<Column>\\d+))?");
 			var regexLine = new Regex ("at (?<MethodName>.*) in (?<FileName>.*):line (?<LineNumber>\\d+)(,(?<Column>\\d+))?");
 			var frames = new List<ObjectValue> ();
-			
+
 			foreach (var sframe in trace.Split ('\n')) {
 				string text = sframe.Trim (' ', '\r', '\t');
 				string file = "";
@@ -200,7 +203,7 @@ namespace Mono.Debugging.Evaluation
 				var fileVal = ObjectValue.CreatePrimitive (null, new ObjectPath ("File"), "", new EvaluationResult (file), ObjectValueFlags.None);
 				var lineVal = ObjectValue.CreatePrimitive (null, new ObjectPath ("Line"), "", new EvaluationResult (line.ToString ()), ObjectValueFlags.None);
 				var colVal = ObjectValue.CreatePrimitive (null, new ObjectPath ("Column"), "", new EvaluationResult (column.ToString ()), ObjectValueFlags.None);
-				var children = new ObjectValue[] { fileVal, lineVal, colVal };
+				var children = new ObjectValue [] { fileVal, lineVal, colVal };
 
 				var frame = ObjectValue.CreateObject (null, new ObjectPath (), "", new EvaluationResult (text), ObjectValueFlags.None, children);
 				frames.Add (frame);
