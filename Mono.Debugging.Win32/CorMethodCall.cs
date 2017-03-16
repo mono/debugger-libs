5db72cd646e0c241a7b5d65eccd618d27eb09e40
Artem Bukhonov <Artem.Bukhonov@jetbrains.com>
2017-03-16 21:55:08 +0300

    CorDebug  Invocations rewritten to  .Net Task API (commit moved from MD repo)

--- Mono.Debugging.Win32/CorMethodCall.cs
+++ Mono.Debugging.Win32/CorMethodCall.cs
@@ -1,47 +1,113 @@
 ﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Samples.Debugging.CorDebug;
+using Microsoft.Samples.Debugging.CorDebug.NativeApi;
 using Mono.Debugging.Evaluation;
 
 namespace Mono.Debugging.Win32
 {
-	class CorMethodCall: AsyncOperation
+	class CorMethodCall: AsyncOperationBase<CorValue>
 	{
-		public delegate void CallCallback ( );
-		public delegate string DescriptionCallback ( );
+		readonly CorEvaluationContext context;
+		readonly CorFunction function;
+		readonly CorType[] typeArgs;
+		readonly CorValue[] args;
 
-		public CallCallback OnInvoke;
-		public CallCallback OnAbort;
-		public DescriptionCallback OnGetDescription;
+		readonly CorEval eval;
 
-		public ManualResetEvent DoneEvent = new ManualResetEvent (false);
+		public CorMethodCall (CorEvaluationContext context, CorFunction function, CorType[] typeArgs, CorValue[] args)
+		{
+			this.context = context;
+			this.function = function;
+			this.typeArgs = typeArgs;
+			this.args = args;
+			eval = context.Eval;
+		}
+
+		void ProcessOnEvalComplete (object sender, CorEvalEventArgs evalArgs)
+		{
+			DoProcessEvalFinished (evalArgs, false);
+		}
+
+		void ProcessOnEvalException (object sender, CorEvalEventArgs evalArgs)
+		{
+			DoProcessEvalFinished (evalArgs, true);
+		}
+
+		void DoProcessEvalFinished (CorEvalEventArgs evalArgs, bool isException)
+		{
+			if (evalArgs.Eval != eval)
+				return;
+			context.Session.OnEndEvaluating ();
+			evalArgs.Continue = false;
+			tcs.TrySetResult(new OperationResult<CorValue> (evalArgs.Eval.Result, isException));
+		}
+
+		void SubscribeOnEvals ()
+		{
+			context.Session.Process.OnEvalComplete += ProcessOnEvalComplete;
+			context.Session.Process.OnEvalException += ProcessOnEvalException;
+		}
+
+		void UnSubcribeOnEvals ()
+		{
+			context.Session.Process.OnEvalComplete -= ProcessOnEvalComplete;
+			context.Session.Process.OnEvalException -= ProcessOnEvalException;
+		}
 
 		public override string Description
 		{
-			get { return OnGetDescription (); }
-		}
-
-		public override void Invoke ( )
-		{
-			OnInvoke ();
-		}
-
-		public override void Abort ( )
-		{
-			OnAbort ();
-		}
-
-		public override void Shutdown ( )
-		{
-			try {
-				Abort ();
+			get
+			{
+				var met = function.GetMethodInfo (context.Session);
+				if (met == null)
+					return "<Unknown>";
+				if (met.DeclaringType == null)
+					return met.Name;
+				return met.DeclaringType.FullName + "." + met.Name;
 			}
-			catch {
-			}
-			DoneEvent.Set ();
 		}
 
-		public override bool WaitForCompleted (int timeout)
+		readonly TaskCompletionSource<OperationResult<CorValue>> tcs = new TaskCompletionSource<OperationResult<CorValue>> ();
+		const int DelayAfterAbort = 500;
+
+		protected override void AfterCancelledImpl (int elapsedAfterCancelMs)
 		{
-			return DoneEvent.WaitOne (timeout, false);
+			if (tcs.TrySetCanceled ()) {
+				// really cancelled for the first time not before. so we should check that we awaited necessary amout of time after Abort() call
+				// else if we return too earle after Abort() the process may be PROCESS_NOT_SYNCHRONIZED
+				if (elapsedAfterCancelMs < DelayAfterAbort) {
+					Thread.Sleep (DelayAfterAbort - elapsedAfterCancelMs);
+				}
+			}
+			context.Session.OnEndEvaluating ();
+		}
+
+		protected override Task<OperationResult<CorValue>> InvokeAsyncImpl (CancellationToken token)
+		{
+			SubscribeOnEvals ();
+
+			if (function.GetMethodInfo (context.Session).Name == ".ctor")
+				eval.NewParameterizedObject (function, typeArgs, args);
+			else
+				eval.CallParameterizedFunction (function, typeArgs, args);
+			context.Session.Process.SetAllThreadsDebugState (CorDebugThreadState.THREAD_SUSPEND, context.Thread);
+			context.Session.ClearEvalStatus ();
+			context.Session.OnStartEvaluating ();
+			context.Session.Process.Continue (false);
+			Task = tcs.Task;
+			// Don't pass token here, because it causes immediately task cancellation which must be performed by debugger event or real timeout 
+			// ReSharper disable once MethodSupportsCancellation
+			return Task.ContinueWith (task => {
+				UnSubcribeOnEvals ();
+				return task.Result;
+			});
+		}
+
+
+		protected override void CancelImpl ( )
+		{
+			eval.Abort ();
 		}
 	}
 }
