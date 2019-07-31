40319d402cf9e6ceae5721c08e096b5e2eca6831
Greg Munn <gregm@microsoft.com>
2019-07-31 12:04:34 -0400

    Nullcheck GetPdbData for Sourcelink

--- Mono.Debugging.Soft/SoftDebuggerBacktrace.cs
+++ Mono.Debugging.Soft/SoftDebuggerBacktrace.cs
@@ -187,7 +187,7 @@ namespace Mono.Debugging.Soft
 					external = method.GetCustomAttributes (nonUserCodeAttr, true).Any ();
 				}
 			}
-			var sourceLink = session.GetPdbData (frame.Method).GetSourceLink(frame.FileName);
+			var sourceLink = session.GetPdbData (frame.Method)?.GetSourceLink(frame.FileName);
 
 			var location = new DC.SourceLocation (methodName, fileName, frame.LineNumber, frame.ColumnNumber, frame.Location.EndLineNumber, frame.Location.EndColumnNumber, frame.Location.SourceFileHash, sourceLink);
 
